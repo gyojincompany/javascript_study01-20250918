2e74661cafb0a407756ebbdab91e1ac7fc94dfb1
gyojincompany <luckyred@gmail.com>
2025-09-23 16:38:00 +0900

    ajax 예제 추가

---
+++ 자바스크립트_10장_연습파일/js/example.js
@@ -0,0 +1,8 @@
+document.write("helloWorld!!\"헬로우월드!!\"");
+
+var str1 = "kor";
+var str2 = 'jap';
+var num1 = 100;
+var num2 = Number("100"); //문자 100 -> 숫자 100
+var tag1 = "<h1> String </h1>";
+var bool1 = Boolean(null); //false -> 0과 null이 아닌 값은 모두 true
